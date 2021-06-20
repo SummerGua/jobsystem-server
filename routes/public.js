b7b72cd960726af0845b3b712074fe0ffbb32ec8
SummerGua <1363106651@qq.com>
2021-06-20 16:14:54 +0800

    little

--- routes/public.js
+++ routes/public.js
@@ -14,29 +14,27 @@ router.get('/getComs', (req, res) => { //首页的所有公司
   })
 })
 router.get('/userGetJobs', (req, res) => { //点击公司查看该公司岗位
-    let cid = req.query.cid
-    let sql = `select jid,jobname,salary,city,diploma,exp,description,skills,uid,upTime from jobs where cid=${cid}`
-    db.query(sql, (err, result) => {
-      if(err) throw err
-      let response = {
-        code: 0,
-        data: result
-      }
-      res.send(JSON.stringify(response))
-    })
+  let cid = req.query.cid
+  let sql = `select jid,jobname,salary,city,diploma,exp,description,skills,uid,upTime from jobs where cid=${cid}`
+  db.query(sql, (err, result) => {
+    if(err) throw err
+    let response = {
+      code: 0,
+      data: result
+    }
+    res.send(JSON.stringify(response))
+  })
 })
 
 router.get('/getOneCom', (req, res) => {
-  console.log('123123')
-  console.log('123123'+req.query.cid)
-    let sql = `select * from companyinfo where cid=${req.query.cid}`
-    db.query(sql, (err, result) => {
-      if(err) throw err
-      let response = {
-        code: 0,
-        data: result
-      }
-      res.send(JSON.stringify(response))
+  let sql = `select * from companyinfo where cid=${req.query.cid}`
+  db.query(sql, (err, result) => {
+    if(err) throw err
+    let response = {
+      code: 0,
+      data: result
+    }
+    res.send(JSON.stringify(response))
   })
 })
 
